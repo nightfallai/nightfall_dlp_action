3375610f319aef587f6ebe75c82d306a393e8b4c
Alan Yan <alan@nightfall.ai>
2020-08-04 13:08:19 -0400

    Fix Initial Commit Get Diff (#11)
    * fix initial commit diff
    
    * add echos
    
    * add echos to sh
    
    * echo pr head sha
    
    * add case for when initial commit
    
    * fix syntax
    
    * add echo
    
    * fetch HEAD on initial commit
    
    * echo both fetch from HEAD and master
    
    * using git show to get diff on initial commit
    
    * echo git diff with show on initial commit
    
    * rm echos
    
    * put echos back to test initial PR
    
    * rm echos
    
    * test if this works
    
    * no version
    
    * setting action version with var
    
    * rm env prefix
    
    * add ref as step
    
    * test setting env var
    
    * one more try
    
    * update action pointer to branch
    
    * echo head ref
    
    * try pointing to action v head ref
    
    * try different syntax
    
    * fix var
    
    * set env in prev step
    
    * set action version env
    
    * try echoing
    
    * try this
    
    * change syntax
    
    * revert

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,15 @@ if [ "$GITHUB_BASE_REF" ]; then
   echo "PR: fetching diff between origin/$GITHUB_BASE_REF and $GITHUB_SHA";
   git diff origin/"$GITHUB_BASE_REF" "$GITHUB_SHA" > $diff_filename;
 else
-  git fetch origin "$EVENT_BEFORE" --depth=1;
-  echo "PUSH: fetching diff between $EVENT_BEFORE and $GITHUB_SHA";
-  git diff "$EVENT_BEFORE" "$GITHUB_SHA" > $diff_filename;
+  if [ "$EVENT_BEFORE" = "0000000000000000000000000000000000000000" ]; then
+    echo "PUSH: fetching diff of initial commit";
+    git show --format="" "$GITHUB_SHA" > $diff_filename;
+  else
+    git fetch origin "$EVENT_BEFORE" --depth=1;
+    echo "PUSH: fetching diff between $EVENT_BEFORE and $GITHUB_SHA";
+    git diff "$EVENT_BEFORE" "$GITHUB_SHA" > $diff_filename;
+  fi;
 fi;
 
 # run nightfalldlp binary
 "$GOPATH/bin/nightfalldlp"
-
